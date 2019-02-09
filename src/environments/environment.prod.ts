52b749fc4c9a1710911eeac8a0c9d33d00479b3b
Aravind <aravindharigoppula@gmail.com>
2019-02-09 09:56:13 -0500

    initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCLkqHfYPbYlJtRkGl6Y42SNXlV9NACWyc",
+    authDomain: "onlineshop-343ee.firebaseapp.com",
+    databaseURL: "https://onlineshop-343ee.firebaseio.com",
+    projectId: "onlineshop-343ee",
+    storageBucket: "onlineshop-343ee.appspot.com",
+    messagingSenderId: "674774807400"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCLkqHfYPbYlJtRkGl6Y42SNXlV9NACWyc",
+    authDomain: "onlineshop-343ee.firebaseapp.com",
+    databaseURL: "https://onlineshop-343ee.firebaseio.com",
+    projectId: "onlineshop-343ee",
+    storageBucket: "onlineshop-343ee.appspot.com",
+    messagingSenderId: "674774807400"
+  }
 };
 
 /*
