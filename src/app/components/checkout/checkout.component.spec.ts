8b9462022dcdd4a1d54e9fb370958f36b6831b21
Aravind <aravindharigoppula@gmail.com>
2019-02-11 09:15:24 -0500

    added components, routing and few navigation links on nav bar

---
+++ src/app/components/checkout/checkout.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { CheckOutComponent } from './checkout.component';
+
+describe('CheckoutComponent', () => {
+  let component: CheckOutComponent;
+  let fixture: ComponentFixture<CheckOutComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ CheckOutComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CheckOutComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
