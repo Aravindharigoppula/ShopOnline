caf5e424b211f19467b10ca4704c02088fc227ca
Aravind <aravindharigoppula@gmail.com>
2019-02-11 08:55:44 -0500

    added components, routing and few links on nav bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from 'src/app/components/home/home.component';
+import { ShoppingCartComponent } from 'src/app/components/shopping-cart/shopping-cart.component';
+import { CheckOutComponent } from 'src/app/components/checkout/checkout.component';
+import { OrderSuccessComponent } from 'src/app/components/order-success/order-success.component';
+import { MyOrdersComponent } from 'src/app/components/my-orders/my-orders.component';
+import { LoginComponent } from 'src/app/components/login/login.component';
+import { AdminProductsComponent } from 'src/app/components/admin-products/admin-products.component';
+import { AdminOrdersComponent } from 'src/app/components/admin-orders/admin-orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path : '', component :  HomeComponent },
+  {path : 'shopping-cart', component : ShoppingCartComponent},
+  {path : 'check-out', component : CheckOutComponent},
+  {path : 'order-success',  component : OrderSuccessComponent},
+  {path : 'my/orders' , component : MyOrdersComponent},
+  {path: 'login', component : LoginComponent },
+  {path: 'admin/products' , component : AdminProductsComponent},
+  {path: 'admin/orders', component : AdminOrdersComponent },
+  {path : '**', component : HomeComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,37 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './components/bs-navbar/bs-navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { ProductsComponent } from './components/products/products.component';
+import { CheckOutComponent } from 'src/app/components/checkout/checkout.component';
+import { OrderSuccessComponent } from './components/order-success/order-success.component';
+import { LoginComponent } from './components/login/login.component';
+import { AdminProductsComponent } from './components/admin-products/admin-products.component';
+import { AdminOrdersComponent } from './components/admin-orders/admin-orders.component';
+import { MyOrdersComponent } from './components/my-orders/my-orders.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,
+    HomeComponent,
+    ShoppingCartComponent,
+    ProductsComponent,
+    CheckOutComponent,
+    OrderSuccessComponent,
+    LoginComponent,
+    AdminProductsComponent,
+    AdminOrdersComponent,
+    MyOrdersComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
